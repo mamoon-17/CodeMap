1048ca867979cb40396ad0a45b75308912e9924b
haziqbilall <haziqbilal7@gmail.com>
2026-03-11 16:40:52 +0500

    CORS Update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,12 @@ import projectRoutes from "./modules/project/project.routes";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
